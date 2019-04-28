6dfebd314e09adfc9853688fcdb511ee28f9b17a
bluebear044 <bluebear044@gmail.com>
2019-04-29 00:33:05 +0900

    add jsoup

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 ```
 --windows
 javac -encoding UTF-8 -cp ./lib/*;. Repeater.java
-java -cp ./lib/client-combined-3.141.59.jar;./lib/byte-buddy-1.8.15.jar;./lib/commons-exec-1.3.jar;./lib/guava-25.0-jre.jar;./lib/okhttp-3.11.0.jar;./lib/okio-1.14.0.jar;. Repeater
+java -cp ./lib/*;. Repeater
 ```
 
-참고 : <https://beomi.github.io/2017/02/27/HowToMakeWebCrawler-With-Selenium/>
+참고 : 
+<https://beomi.github.io/2017/02/27/HowToMakeWebCrawler-With-Selenium>
+<https://www.popit.kr/web-scraping-by-selenium>
+<https://stackoverflow.com/questions/15656252/wait-till-text-present-in-text-field>
--- Repeater.java
+++ Repeater.java
@@ -6,6 +6,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.Set;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.openqa.selenium.Cookie;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
  
 public class Repeater {
 
@@ -15,7 +22,7 @@ public class Repeater {
 		String loginPW = "패스워드";
 		String floor = "F80090125";
 		String section = "S80092503";
-		String seatNO = "s40";
+		String seatNO = "1045";
  
 		Repeater instance = new Repeater();
 		instance.repeat(loginID, loginPW, floor, section, seatNO);
@@ -60,12 +67,24 @@ public class Repeater {
 			webElement.sendKeys(loginPW);
 
 			driver.findElement(By.xpath("//*[@id='lnkDoLogin']")).click();
+
+			if(driver.findElements(By.id("lblAuthMessage")).size() == 0) {
+
+				System.out.println("[LOG] Login Failed");
+				return;
+
+			}else {
+
+				System.out.println("[LOG] Login Success");
+				
+			}
 			
 			//System.out.println(driver.getPageSource());
 
 			if(driver.findElements(By.xpath("//*[@id='body_divResCancelBtn']/div/button")).size() != 0){
 				
 				System.out.println("[LOG] Already reserved");
+				return;
 
 			}else {
 				
@@ -80,21 +99,55 @@ public class Repeater {
 				sectionSelect.selectByValue(section);
 
 				System.out.println("[LOG] Selected section");
-
+			
 				//add Explicit Wait
-				//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='" + seatNO + "']")));
-				//driver.findElement(By.xpath("//*[@id='" + seatNO + "']")).click();
+				wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@data-seatno='" + seatNO + "']")));
+				WebElement element = driver.findElement(By.xpath("//*[@data-seatno='" + seatNO + "']"));
 
+				if(element.getAttribute("data-flag").equals("true")) {
+
+					System.out.println("[LOG] Seat unavailable");
+					return;
+
+				}
+
+				System.out.println(element.getAttribute("data-seatno"));
+				System.out.println(element.getAttribute("data-flag"));
+
+				//wait.until(ExpectedConditions.elementSelectionStateToBe(By.id("//*[@id='body_ddlSch_section']/option[3]")));
+				//driver.findElement(By.id("s40")).click();
 				//System.out.println("[LOG] Selected seat");
 
-				wait.until(ExpectedConditions.elementToBeClickable(By.id("s40")));
-				driver.findElement(By.id("s40")).click();
-			    driver.findElement(By.cssSelector("div:nth-child(1) > button:nth-child(3)")).click();
-			    driver.findElement(By.cssSelector(".w3-button")).click();
+				// 로그인 Cookies 얻어 옴
+				Set<Cookie> cookies = driver.manage().getCookies();
+				Map<String, String> cookieMap = cookies.stream().collect(Collectors.toMap(Cookie::getName, Cookie::getValue));
+				for (Map.Entry<String, String> entry : cookieMap.entrySet()) {
+				    System.out.println("key: " + entry.getKey() + ", value : " + entry.getValue());
+				}
+
+				Document doc = Jsoup.connect("http://sor.lotte.com/htm/web/reservation/res_today.aspx")
+					.data("hdfSelectSeat", "1045")
+					.data("hdfResSeq", "")
+					.data("ddlSch_floor", "F80090125")
+					.data("ddlSch_section", "S80092503")
+					.data("txtNowDate", "2019-04-28")
+					.data("btnResAppr", "")
+					.userAgent("Mozilla")
+					.cookies(cookieMap) // 로그인 Cookies 세팅
+					.post();
+				System.out.println(doc);
+
+				//url : http://sor.lotte.com/htm/web/reservation/res_today.aspx
+				//ctl00$ScriptManager: ctl00$UpdatePanel|ctl00$body$btnResAppr
+				//ctl00$body$hdfSelectSeat: 1045
+				//ctl00$body$hdfResSeq: 
+				//ctl00$body$ddlSch_floor: F80090125
+				//ctl00$body$ddlSch_section: S80092503
+				//ctl00$body$txtNowDate: 2019-04-28
+				//ctl00$body$btnResAppr:
 
 				//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='body_divResApprBtn']/div/button")));
 				//driver.findElement(By.xpath("//*[@id='body_divResApprBtn']/div/button")).click();
-				
 			}
 
 			Thread.sleep(10000);
